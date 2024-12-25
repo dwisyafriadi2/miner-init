44b84afa41cbbdeb66dbee862f5927a2ba00a323
dwisyafriadi2 <dwisyafriadi15@gmail.com>
2024-12-25 12:13:00 +0700

    Update Windows

--- iniminer-windows-x64/start.bat
+++
@@ -1,1 +0,0 @@
-iniminer.exe --pool stratum+tcp://0x0304f5193FCe6A27e3789c27EE2B9D95177e21A5.Worker001@pool-core-testnet.inichain.com:32672 --cpu-devices 0 --cpu-devices 1 --cpu-devices 2 --cpu-devices 3
